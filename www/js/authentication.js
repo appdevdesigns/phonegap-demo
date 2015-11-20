c6a7a4f015e3d3062bfc0d89d759597e8c2f0dc4
Joshua Chan <joshua@appdevdesigns.net>
2015-11-20 05:38:13 -0800

    + Authentication via CAS RESTful interface

--- www/js/authentication.js
+++ www/js/authentication.js
@@ -6,29 +6,91 @@ import Config from './config';
 
 const Authentication = {
     login: function (username, password) {
+        var self = this;
         var serverURL = Config.getServer();
-
-
-        return $.ajax({url:serverURL+'/csrfToken'})
-            .then((data) => {
-                var csrf = data._csrf;
-                return $.ajax({
-                        url:serverURL+'/site/login', 
-                        method: 'POST', 
+        var authType = 'CAS';
+        
+        var dfd = $.Deferred();
+        
+        $.ajax({url:serverURL+'/csrfToken'})
+        .then((data) => {
+            var csrf = data._csrf;
+            
+            // RESTful CAS interface
+            if (authType == 'CAS') {
+                // Step 1: Get the TGT
+                $.ajax({
+                    url: serverURL+'/cas/v1/tickets',
+                    method: 'POST',
+                    data: {
+                        _csrf: csrf,
+                        username: username,
+                        password: password
+                    }
+                })
+                .fail((err) => {
+                    console.log('Login failed', err);
+                    dfd.reject(err);
+                })
+                .done((data, textStatus, res) => {
+                    // Step 2: Use the TGT to get a ticket
+                    var tgtURL = res.getResponseHeader('Location');
+                    var serviceURL = serverURL + '/site/login-done';
+                    $.ajax({
+                        url: tgtURL,
+                        method: 'POST',
                         data: {
-                          _csrf: csrf,
-                          username: username, 
-                          password: password
+                            _csrf: csrf,
+                            service: serviceURL
                         }
                     })
-                    .then((value) => {
-                        this.dispatch('loggedIn');
-                        return value;
-                    })
                     .fail((err) => {
-                        console.log('Yikes!', err);
+                        console.log('Failed to get CAS ticket', err);
+                        dfd.reject(err);
+                    })
+                    .done((ticket) => {
+                        // Step 3: Use the ticket to get the session cookie
+                        $.ajax({
+                            url: serviceURL,
+                            method: 'GET',
+                            data: { ticket: ticket }
+                        })
+                        .fail((err) => {
+                            console.log('Failed to validate CAS ticket', err);
+                            dfd.reject(err);
+                        })
+                        .done(() => {
+                            self.dispatch('loggedIn');
+                            dfd.resolve();
+                        });
                     });
-            });
+                });
+            }
+            
+            // Local auth
+            else {
+                $.ajax({
+                    url:serverURL+'/site/login', 
+                    method: 'POST', 
+                    data: {
+                      _csrf: csrf,
+                      username: username, 
+                      password: password
+                    }
+                })
+                .then((value) => {
+                    this.dispatch('loggedIn');
+                    dfd.resolve();
+                    return value;
+                })
+                .fail((err) => {
+                    console.log('Yikes!', err);
+                    dfd.reject(err);
+                });
+            }
+        });
+        
+        return dfd;
     }
 }
 
