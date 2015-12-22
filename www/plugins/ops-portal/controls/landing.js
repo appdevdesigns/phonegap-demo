53840c2e9ec7ec5ebcf79101de22d7b4dc822639
Joshua Chan <joshua@appdevdesigns.net>
2015-12-22 02:04:14 -0800

    * Fixed graph rendering bug that could happen if the Landing page was hidden before it finished loading

--- www/plugins/ops-portal/controls/landing.js
+++ www/plugins/ops-portal/controls/landing.js
@@ -17,10 +17,14 @@ export default Page.extend('LandingControl', {
 
     // Initialize the control scope and render it
     this.checkServer();
+    
+    /*
     setInterval(() => {
       //Every X seconds
       this.checkServer();
     }, 20000);
+    */
+    
     Config.on('serverChanged', (newURL) => {
         this.scope.attr('serverErr', false);
     });
@@ -38,7 +42,6 @@ export default Page.extend('LandingControl', {
     if (currentServer) {
       Config.loadConfig(currentServer).then(() => {
         //Success, no need to warn anyone
-        //console.log('Server good');
         this.scope.attr('serverErr', false);
       })
       .fail(err => {
@@ -60,9 +63,9 @@ export default Page.extend('LandingControl', {
         thousandsSep: ',',
       },
     });
-
+    
     AccountModel.findAll()
-      .fail(function (err) {
+      .fail((err) => {
         console.log(err);
       })
       .then((list) => {
@@ -75,8 +78,8 @@ export default Page.extend('LandingControl', {
         income /= list.length;
         expenses /= list.length;
         
-        this.element.find('#average').highcharts(
-          {
+        const chartContainer = this.element.find('#average');
+        const chartOptions = {
             colors: ['#3D9D50', '#DB1A23', '#000'],
             chart: {
               type: 'column',
@@ -143,12 +146,21 @@ export default Page.extend('LandingControl', {
                 
               },
             ],
-          }
-        );
+        };
+        
+        // Don't init the chart until the container is visible.
+        // Current page may have changed while data was loading.
+        if (chartContainer.is(':visible')) {
+            chartContainer.highcharts(chartOptions);
+        } else {
+            this.element.one('pageshow', () => {
+                chartContainer.highcharts(chartOptions);
+            });
+        }
     });
   },
 
-  "#average click" : function(element, event){
+  '#average click'(element, event) {
     Navigator.openPage("account");
   },
 
