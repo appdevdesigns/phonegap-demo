121c459c235fccd3cc0958cd7f3d3a03b3eb2260
Joshua Chan <joshua@appdevdesigns.net>
2015-12-22 00:10:35 -0800

    * Comm.request() now standardized to use jQuery Deferred * Fixed minor bug with automatic request queueing during reauthentication * Fixed minor bug with automatic CSRF token fetching

--- www/js/comm.js
+++ www/js/comm.js
@@ -18,7 +18,7 @@ const Server = {
  * CSRF object for internal use only.
  */
 const CSRF = {
-    token: 'null',
+    token: null,
     /**
      * Fetch the user's CSRF token from sails.js
      * @return Deferred
@@ -32,15 +32,14 @@ const CSRF = {
             dataType: 'json',
             cache: false
         })
-        .done(function(data, status, res){
+        .done((data, status, res) => {
             CSRF.token = data._csrf;
             dfd.resolve(CSRF.token);
         })
-        .fail(function(res, status, err){
+        .fail((res, status, err) => {
             var csrfError = new Error('Unable to get CSRF token: ' + err.message);
             console.log(csrfError);
             dfd.reject(csrfError);
-            //dfd.resolve(null);
         });
         return dfd;
     }
@@ -59,9 +58,13 @@ const Comm = {
    * @param object options
    * @param Deferred dfd
    *    Optional. By default a new Deferred will be created.
+   * @return jQuery Deferred
    */
   request(options, dfd) {
     dfd = dfd || $.Deferred();
+    
+    // For compatibility with Promise
+    dfd.catch = dfd.fail;
 
     const { url, method, params, retryFailures } = options;
 
@@ -75,14 +78,13 @@ const Comm = {
     // Fetch CSRF token if needed
     if (!CSRF.token && (!method || method !== 'GET')) {
         CSRF.fetch()
-        .done(function(){
+        .fail(dfd.reject)
+        .done(() => {
             // Resubmit request after getting token
-            Comm.request(options)
-            .then(dfd.resolve)
-            .catch(dfd.reject);
-        })
-        .fail(dfd.reject);
-        return Promise.resolve(dfd).catch(function(err){});
+            Comm.request(options, dfd);
+        });
+        //return Promise.resolve(dfd).catch(function(err){});
+        return dfd;
     }
 
     // add the CSRF token
@@ -97,13 +99,12 @@ const Comm = {
             options: options,
             dfd: dfd
         });
-        return;
     }
     else {
         // now send the request:
         $.ajax(ajaxOptions)
-        .fail(function(res, status, statusText){
-            if (retryFailures) {
+        .fail((res, status, statusText) => {
+            if (0 && retryFailures) {
               options.retryFailures = false;
               const queuedRequest = new HTTPRequest({
                 options: options,
@@ -113,15 +114,11 @@ const Comm = {
             }
     
             // was this a CSRF error?
-            if (res.responseText.toLowerCase().indexOf('csrf') != -1) {
-    
+            else if (res.responseText.toLowerCase().indexOf('csrf') != -1) {
                 // reset our CSRF token
                 CSRF.token = null;
-    
                 // resubmit the request 
-                Comm.request(options)
-                .then(dfd.resolve)
-                .catch(dfd.reject);
+                Comm.request(options, dfd);
                 return;
             }
             // Need to reauthenticate?
@@ -130,78 +127,49 @@ const Comm = {
                     options: options,
                     dfd: dfd,
                 });
-                Navigator.push();
-                Navigator.openPage('login');
+                if (!authInProgress) {
+                    authInProgress = true;
+                    Navigator.push();
+                    Navigator.openPage('login');
+                }
                 return;
             }
-    
-    
-            // check to see if responseText is our json response
-            var data = null;
-            try { data = JSON.parse(res.responseText); } catch(e) {}
-            // if (('object' == typeof data) && (data != null)) {
-    
-            //     if ('undefined' != typeof data.status) {
-    
-            //         // this could very well be one of our messages:
-            //         _handleAppdevError( data );
-            //         return;
-            //     };
-            // }
-            
-            dfd.reject()
-            
-    
+            else {
+                // check to see if responseText is our json response
+                var data = null;
+                try { data = JSON.parse(res.responseText); } 
+                catch(e) { data = { message: res.responseText }; }
+                dfd.reject(data);
+            }
         })
-        .done(function(data, textStatus, req){
+        .done((data, textStatus, req) => {
     
             // Got a JSON response but was the service response an error?
             if (data.status && (data.status == 'error')) {
-    
-    console.log('***** appdev error received!', data);
-    
-                // _handleAppdevError(data);
-    dfd.reject(data);
+                console.log('***** appdev error received!', data);
+                dfd.reject(data);
                 return;
             }
             // Success!
             else {
-    
                 // if this was an appdev packet, only return the data:
                 if (data.status && data.status == 'success') {
                     data = data.data;
                 }
-    
                 dfd.resolve(data);
             }
     
         })
     }
 
-
-
-    return Promise.resolve(dfd);
-
-
-    // // Convert the "thenable" returned by jQuery's ajax method to a normal Promise instance
-    // return Promise.resolve($.ajax(ajaxOptions)).catch(err => {
-    //   // Send to login page in case of 401
-    //   if (err.status === 401 || err.status === 403) {
-    //     Navigator.openPage('login');
-    //     pendingAuthentication.push(options);
-    //   } else if (retryFailures) {
-    //     const queuedRequest = new HTTPRequest({
-    //       options: ajaxOptions,
-    //     });
-    //     queuedRequest.save();
-    //   }
-    // });
+    //return Promise.resolve(dfd);
+    return dfd;
   },
 };
 
 Authentication.on('loggedIn', () => {
   authInProgress = false;
-  // Resubmit all queued requests, to be resolved with their original DFD
+  // Resubmit all queued requests, to be resolved with their original Deferred
   pendingAuthentication.forEach(request => { 
     Comm.request(request.options, request.dfd);
   });
